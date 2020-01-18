a0ccf354dadc24fb2077cee24610e12ec15bc669
Sergei <exemple@gmail.com>
2020-01-18 13:25:25 +0200

    adddz number7

--- scriptdz.js
+++ scriptdz.js
@@ -1,3 +1,32 @@
+'use strict';
+
+let startCalc = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    ptionalexpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
+
+console.log(optionalexpensesItem);
+
 let money, time;
 
 function start() {
@@ -86,8 +115,7 @@ let appData = {
                 console.log('bad result');
                 i--;
             }
-        }
-        
+        }  
     }
 }
 
